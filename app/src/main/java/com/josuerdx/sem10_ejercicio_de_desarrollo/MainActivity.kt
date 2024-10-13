e5a6589e54f967ab1d9dfac75d67c36c1670393f
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-10-13 13:02:22 -0500

    feat: configure navigation and add navigation-compose dependency

--- app/src/main/java/com/josuerdx/sem10_ejercicio_de_desarrollo/MainActivity.kt
+++ app/src/main/java/com/josuerdx/sem10_ejercicio_de_desarrollo/MainActivity.kt
@@ -3,45 +3,24 @@ package com.josuerdx.sem10_ejercicio_de_desarrollo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.josuerdx.sem10_ejercicio_de_desarrollo.ui.theme.Sem10_Ejercicio_de_desarrolloTheme
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.josuerdx.sem10_ejercicio_de_desarrollo.view.ProductDetailScreen
+import com.josuerdx.sem10_ejercicio_de_desarrollo.view.ProductListScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Sem10_Ejercicio_de_desarrolloTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            val navController = rememberNavController()
+            NavHost(navController, startDestination = "product_list") {
+                composable("product_list") { ProductListScreen() }
+                composable("product_detail/{productId}") { backStackEntry ->
+                    val productId = backStackEntry.arguments?.getString("productId")?.toInt() ?: 0
+                    ProductDetailScreen(productId)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Sem10_Ejercicio_de_desarrolloTheme {
-        Greeting("Android")
     }
 }
