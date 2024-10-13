de91dabe94eec0fcda811ca16b3e14d155bae6f1
RDx <rodrigo.apaza.m@tecsup.edu.pe>
2024-10-13 12:39:43 -0500

    feat: add ProductListScreen with Jetpack Compose

---
+++ app/src/main/java/com/josuerdx/sem10_ejercicio_de_desarrollo/view/ProductListScreen.kt
@@ -0,0 +1,43 @@
+package com.josuerdx.sem10_ejercicio_de_desarrollo.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.josuerdx.sem10_ejercicio_de_desarrollo.data.Product
+import com.josuerdx.sem10_ejercicio_de_desarrollo.network.RetrofitClient
+import kotlinx.coroutines.launch
+
+@Composable
+fun ProductListScreen() {
+    val productList = remember { mutableStateListOf<Product>() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            val products = RetrofitClient.instance.getAllProducts()
+            productList.addAll(products)
+        }
+    }
+
+    LazyColumn(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+        items(productList) { product ->
+            ProductItem(product = product)
+        }
+    }
+}
+
+@Composable
+fun ProductItem(product: Product) {
+    Card(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "Title: ${product.title}")
+            Text(text = "Price: $${product.price}")
+            Text(text = "Category: ${product.category}")
+        }
+    }
+}
